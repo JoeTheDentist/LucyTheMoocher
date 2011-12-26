1d7e104f59493d4ed4657619fa471a5f3251b519
berardg <berardg@ensimag.fr>
2011-12-26 13:45:24 +0100

    sons, transition pas terrible

--- LucyTheMoocher/src/com/lucythemoocher/sounds/SoundManager.java
+++ LucyTheMoocher/src/com/lucythemoocher/sounds/SoundManager.java
@@ -5,24 +5,31 @@ import java.util.HashMap;
 import java.util.Vector;
 
 import com.lucythemoocher.R;
+import com.lucythemoocher.Globals.Globals;
 import com.lucythemoocher.util.Resources;
 
 import android.content.Context;
 import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.media.SoundPool;
+import android.util.Log;
 
 public class SoundManager {
-	
+
 	private static final int BACKGROUND_THEME = 0;
 	private static final int BACKGROUND_LVL1 = 1;
 	private static final int BACKGROUND_LVL2 = 2;
 	private static final int BACKGROUND_LVL3 = 3;
-	
+
 	private SoundPool sounds_ = null;
 	private HashMap<Integer, Integer> loaded_ = null;
 	private HashMap<Integer, Integer> playing_ = null;
 	private Vector<ArrayList<Integer>> soundsBackground_;
-	
+	private int sampleDuration_;
+	private int slots_;
+	private float lastPlay_;
+	private boolean started_;
+
 	/**
 	 * Class aimed to play sounds\n
 	 * Warning : I use SoundPool to play the music, this is bad because SoundPool
@@ -35,16 +42,22 @@ public class SoundManager {
 		loaded_ = new HashMap<Integer, Integer>();
 		playing_ = new HashMap<Integer, Integer>();
 		soundsBackground_ = new Vector<ArrayList<Integer>>(4);
-		
+		started_ = false;
+
+		//Calculate the sample duration
+		MediaPlayer mp = MediaPlayer.create(Resources.getActivity(), R.raw.theme1);
+		sampleDuration_ = mp.getDuration();
+
 		//Allocation
 		for ( int i = 0; i<soundsBackground_.capacity(); i++) {
 			ArrayList<Integer> l = new ArrayList<Integer>();
 			soundsBackground_.add(i, l);
 		}
-		
+
 		//Chemins des fichiers de son
 		soundsBackground_.get(BACKGROUND_THEME).add(R.raw.theme1);
-		
+		soundsBackground_.get(BACKGROUND_THEME).add(R.raw.theme2);
+
 		//Ici on charge les sons
 		for ( ArrayList<Integer> l : soundsBackground_ ) {
 			for ( int i : l ) {
@@ -52,45 +65,39 @@ public class SoundManager {
 			}
 		}
 	}
-	
+
 	public void update() {
-		
-	}
-	
-	private int getChannel(int resource) {
-		int i = 0;
-		for ( ArrayList<Integer> l : soundsBackground_ ) {
-			if ( l.contains(resource) ) {
-				break;
+		if ( started_ ) {
+			if ( Globals.getInstance().getTimer().getTime()-lastPlay_ >= sampleDuration_ ) {
+				if ( slots_%2 == 0 ) {
+					Log.d("SoundsManager", "plop"+slots_);
+					playSound(R.raw.theme1);
+				} else {
+					Log.d("SoundsManager", "plip"+slots_);
+					playSound(R.raw.theme2);
+				}
+				lastPlay_ = Globals.getInstance().getTimer().getTime();
+				slots_++;
 			}
-			i++;
 		}
-		return i;
 	}
-	
+
+	public void start() {
+		slots_ = 0;
+		lastPlay_ = -sampleDuration_;
+		started_ = true;
+	}
+
 	private void addSound(int resource) {
-		loaded_.put(getChannel(resource), sounds_.load(Resources.getActivity(), resource, 1));
+		loaded_.put(resource, sounds_.load(Resources.getActivity(), resource, 1));
 	}
-	
+
 	private void playSound(int resource) {
 		AudioManager mgr = (AudioManager)Resources.getActivity().getSystemService(Context.AUDIO_SERVICE);
 		float streamVolumeCurrent = mgr.getStreamVolume(AudioManager.STREAM_MUSIC);
 		float streamVolumeMax = mgr.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
 		float volume = streamVolumeCurrent / streamVolumeMax;
 		//TODO dernier arg => vitesse du jeu
-		playing_.put(getChannel(resource), sounds_.play(loaded_.get(resource), volume, volume, 1, 0, 1f));
-	}
-	
-	private void playLoopSound(int resource) {
-		AudioManager mgr = (AudioManager)Resources.getActivity().getSystemService(Context.AUDIO_SERVICE);
-		float streamVolumeCurrent = mgr.getStreamVolume(AudioManager.STREAM_MUSIC);
-		float streamVolumeMax = mgr.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-		float volume = streamVolumeCurrent / streamVolumeMax;
-		//TODO dernier arg => vitesse du jeu
-		playing_.put(getChannel(resource), sounds_.play(loaded_.get(getChannel(resource)), volume, volume, 1, -1, 0.75f));
-	}
-	
-	public void start() {
-		playLoopSound(R.raw.theme1);
+		playing_.put(resource, sounds_.play(loaded_.get(resource), volume, volume, 1, 0, 1f));
 	}
 }
